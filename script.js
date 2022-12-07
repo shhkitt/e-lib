dc414bf4db69e8ccfa3b6cc4a630e66956f292bd
shhkitt <53077521+shhkitt@users.noreply.github.com>
2022-12-07 23:57:28 +0300

    add project files

---
+++ script.js
@@ -0,0 +1,11 @@
+function myFunction1() {
+  document.getElementById("myDropdown1").classList.toggle("show");
+}
+
+function myFunction2() {
+  document.getElementById("myDropdown2").classList.toggle("show");
+}
+
+function myFunction3() {
+  document.getElementById("myDropdown3").classList.toggle("show");
+}
